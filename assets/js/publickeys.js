7d27768d331cb502ad3ce516129eb6289830b60e
ugopreneur <ugnwokoro@yahoo.com>
2023-02-10 00:28:53 +0100

    switched our private API keys for public ones to be used on deployed application

--- assets/js/publickeys.js
+++ assets/js/publickeys.js
@@ -1,2 +1,3 @@
 var quizApiKey = "Q46GyjOpDNOALdaLsEqh7yvzzOrYdefqcWnBZJWC";
 var translatorApiKey = "1684405318msh8baac8d1e5b384dp1748f4jsnc4ab547afa5b";
+// these keys are public, so they can be used by anyone who visits our deployed application
